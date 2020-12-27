78a3a1ebf94633a9e9b972185b36f554d1d31f75
Aashu_Jha <aashish2000kumar@gmail.com>
2020-12-28 00:45:26 +0530

    refactor: extracted round_icon_button.dart

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -5,7 +5,7 @@ import 'icon_content.dart';
 import 'reusable_card.dart';
 import 'constants.dart';
 import 'bottom_widget.dart';
-
+import 'round_icon_button.dart';
 
 enum Gender {
   male,
@@ -180,24 +180,5 @@ class _InputPageState extends State<InputPage> {
 
 
 
-class RoundIconButton extends StatelessWidget {
-  RoundIconButton({this.icon, @required this.onPress});
-  final IconData icon;
-  final Function onPress;
 
-  @override
-  Widget build(BuildContext context) {
-    return RawMaterialButton(
-      onPressed: onPress,
-      elevation: 5.0,
-      child: Icon(icon),
-      constraints: BoxConstraints.tightFor(
-        width: 56.0,
-        height: 56.0,
-      ),
-      shape: CircleBorder(),
-      fillColor: Color(0xff4C4F5E),
-    );
-  }
-}
 
